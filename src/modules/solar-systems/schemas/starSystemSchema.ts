218a00a6170c964f4be6325c340d340deb30e5ed
Leonardo Yusuke Hirano <leoyuhirano@gmail.com>
2024-10-12 14:27:44 +0900

    feat: validate schema for update star system

--- src/modules/solar-systems/schemas/starSystemSchema.ts
+++ src/modules/solar-systems/schemas/starSystemSchema.ts
@@ -4,3 +4,7 @@ export const createStarSystemSchemaJoi = Joi.object({
 name: Joi.string().min(2).required(),
 description: Joi.string().min(5).required(),
 })
+
+export const updateStarSystemSchemaJoi = Joi.object({
+    name: Joi.string().min(2),
+    description: Joi.string().min(5)})
--- src/routes/starSystem/starSystemRoutes.ts
+++ src/routes/starSystem/starSystemRoutes.ts
@@ -1,6 +1,6 @@
 import { authValidation } from "@/middlewares/authMiddlewares";
 import { validateSchema } from "@/middlewares/schemaValidator";
-import { createStarSystemSchemaJoi } from "@/modules/solar-systems/schemas/starSystemSchema";
+import { createStarSystemSchemaJoi, updateStarSystemSchemaJoi } from "@/modules/solar-systems/schemas/starSystemSchema";
 import createStarSystemController from "@/modules/solar-systems/useCases/create/createController";
 import deleteStarSystemController from "@/modules/solar-systems/useCases/delete/deleteController";
 import findAllcontroller from "@/modules/solar-systems/useCases/findAll/findAllController";
@@ -14,7 +14,7 @@ const starSystemRouters = Router()
 starSystemRouters.post('/',authValidation, validateSchema(createStarSystemSchemaJoi),createStarSystemController)
 starSystemRouters.get('/', findAllcontroller)
 starSystemRouters.get('/:id',findByIdController)
-starSystemRouters.put('/:id',authValidation, updateStarSystemsController)
+starSystemRouters.put('/:id',authValidation,validateSchema(updateStarSystemSchemaJoi), updateStarSystemsController)
 starSystemRouters.delete('/:id',authValidation,deleteStarSystemController)
 
 
